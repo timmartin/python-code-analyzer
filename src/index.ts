b278be187d071c1b20b2e47d82f840bb3acc383d
Tim Martin <tim@asymptotic.co.uk>
2020-04-26 13:35:51 +0100

    Webpack config for a trivial typescript library

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export function doStuff(name: string) {
+  return `Hello ${name}`;
+}
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
