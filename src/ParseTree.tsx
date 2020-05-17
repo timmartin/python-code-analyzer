eb71a9887d5c1fcfb9a630bc173b5e377e214154
Tim Martin <tim@asymptotic.co.uk>
2020-05-17 14:26:22 +0100

    Add react-diagrams for drawing block diagrams
    This doesn't do anything Python-related at the moment, it's just a
    default diagram

---
+++ src/ParseTree.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import createEngine, {
+  DiagramModel,
+  DefaultNodeModel,
+  DefaultLinkModel,
+} from "@projectstorm/react-diagrams";
+import { CanvasWidget } from "@projectstorm/react-canvas-core";
+
+const ParseTree = () => {
+	//1) setup the diagram engine
+	var engine = createEngine();
+
+	//2) setup the diagram model
+	var model = new DiagramModel();
+
+	//3-A) create a default node
+	var node1 = new DefaultNodeModel({
+		name: 'Node 1',
+		color: 'rgb(0,192,255)'
+	});
+	node1.setPosition(100, 100);
+	let port1 = node1.addOutPort('Out');
+
+	//3-B) create another default node
+	var node2 = new DefaultNodeModel('Node 2', 'rgb(192,255,0)');
+	let port2 = node2.addInPort('In');
+	node2.setPosition(400, 100);
+
+	// link the ports
+	let link1 = port1.link<DefaultLinkModel>(port2);
+	link1.getOptions().testName = 'Test';
+	link1.addLabel('Hello World!');
+
+	//4) add the models to the root graph
+	model.addAll(node1, node2, link1);
+
+	//5) load model into engine
+	engine.setModel(model);
+
+  return (
+    <div className="python-analyzer-view parse-tree">
+      <CanvasWidget engine={engine} className="block-diagram-canvas" />
+    </div>
+  );
+};
+
+export default ParseTree;
---
+++ stories/ParseTree.stories.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+import ParseTree from "../src/ParseTree";
+
+export default {
+  title: "Parse tree",
+  component: ParseTree,
+};
+
+export const Default = () => {
+  return <ParseTree />;
+};
