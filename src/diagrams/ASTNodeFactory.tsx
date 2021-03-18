012821d7e84924e9d2a1108a6f3be767040c6544
Tim Martin <tim@asymptotic.co.uk>
2021-03-18 10:21:08 +0000

    Fix an eslint warning

--- src/diagrams/ASTNodeFactory.tsx
+++ src/diagrams/ASTNodeFactory.tsx
@@ -1,16 +1,22 @@
 import * as React from "react";
-import { AbstractReactFactory } from "@projectstorm/react-canvas-core";
+import {
+  AbstractReactFactory,
+  GenerateWidgetEvent,
+} from "@projectstorm/react-canvas-core";
 
 import ASTNodeModel from "./ASTNodeModel";
 import ASTNodeWidget from "./ASTNodeWidget";
 import { DiagramEngine } from "@projectstorm/react-diagrams";
 
-export class ASTNodeFactory extends AbstractReactFactory<ASTNodeModel, DiagramEngine> {
+export class ASTNodeFactory extends AbstractReactFactory<
+  ASTNodeModel,
+  DiagramEngine
+> {
   constructor() {
     super("ast");
   }
 
-  generateReactWidget(event): JSX.Element {
+  generateReactWidget(event: GenerateWidgetEvent<ASTNodeModel>): JSX.Element {
     return <ASTNodeWidget node={event.model} engine={this.engine} />;
   }
 
